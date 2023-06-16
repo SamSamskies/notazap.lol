926dc49cbb1dfb8792498eb6931cd53220e85c1f
Sam Samskies <samsamskies@gmail.com>
2023-06-16 00:28:09 -0500

    add dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,34 @@
 import "@/styles/globals.css";
+import { useEffect, useState } from "react";
 import Head from "next/head";
+import { useDarkMode } from "@/hooks";
+import { DarkModeSwitch } from "react-toggle-dark-mode";
 
 export default function App({ Component, pageProps }) {
+  const [isDarkMode, setDarkMode] = useDarkMode();
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
   return (
     <>
       <Head>
         <title>Not a Zap</title>
       </Head>
+      {hasMounted && (
+        <div
+          style={{ display: "flex", justifyContent: "flex-end", padding: 16 }}
+        >
+          <DarkModeSwitch
+            checked={isDarkMode}
+            onChange={() => setDarkMode((prev) => !prev)}
+            sunColor="#FF9900"
+            moonColor="black"
+          />
+        </div>
+      )}
       <Component {...pageProps} />
     </>
   );
