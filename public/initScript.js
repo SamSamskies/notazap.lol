af03331a95fbe7caac0497c7ac28d1888d944628
Sam Samskies <samsamskies@gmail.com>
2024-08-22 12:40:57 -0500

    bump nostr-zap to latest

--- public/initScript.js
+++ public/initScript.js
@@ -70,7 +70,7 @@ if (typeof window !== "undefined") {
   const renderNostrZap = () => {
     const script = document.createElement("script");
 
-    script.src = "https://cdn.jsdelivr.net/npm/nostr-zap@1.0.1";
+    script.src = "https://cdn.jsdelivr.net/npm/nostr-zap@1.1.0";
     document.body.appendChild(script);
   };
 
